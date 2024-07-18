c0c6f84a0361dccb334f288e5118cde9bbfecdee
anton.firdaus <anton.firdaus@asdtech.co.id>
2024-07-18 12:23:55 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express');
-const router = express.Router();
-const app = express();
+require('dotenv').config()
 
-const tadaController = require("./tada.controller")
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json());
+app.use(cors());
+
+const tadaController = require("./controller/tada.controller")
 
 app.get("/", (req, res) => res.send("Campsys loyalty mock api"));
 app.use('/tada/create', tadaController.create)
